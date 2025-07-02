ca05355ae32753413554228bc10bf992735e58f8
Adarsh Kumar Pathak <122677349+adrs2004@users.noreply.github.com>
2025-07-02 19:15:51 +0530

    Delete TeamApp directory

--- TeamApp/src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-// src/components/ProtectedRoute.jsx
-import { Navigate } from 'react-router-dom';
-
-export default function ProtectedRoute({ children }) {
-  const teamInfo = JSON.parse(localStorage.getItem('teamInfo'));
-  return teamInfo ? children : <Navigate to="/login" />;
-}
