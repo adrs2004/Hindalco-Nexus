79d35625badf4f37f779a8b4bd9494a0c9d282c7
Adarsh Kumar Pathak <122677349+adrs2004@users.noreply.github.com>
2025-07-03 10:50:44 +0530

    Update IssueStatusPage.jsx

--- HindalcoApp/src/pages/IssueStatusPage.jsx
+++ HindalcoApp/src/pages/IssueStatusPage.jsx
@@ -96,7 +96,7 @@ export default function IssueStatusPage() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
+    <div className="min-h bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
       <div className="max-w-6xl mx-auto">
         {/* Geometric header */}
         <div className="relative mb-8 sm:mb-12">
